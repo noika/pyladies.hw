4d47e43c54e8674ec5dd93befc92ff810fb966cf
Nora Kořánová <nora.koranova@gmail.com>
2017-04-17 14:57:52 +0200

    rográmek zjistí jestli jsem kluk nebo holka

---
+++ 206pohlavi.py
@@ -0,0 +1,12 @@
+jmeno = input("Zadej své křestní jméno:")
+
+if jmeno[-2:] == "ie":
+	print("Jsi holka")
+elif jmeno[-1] == "a":
+	print("Jsi holka")
+elif jmeno[-1] == "e":
+	print("Jsi holka")
+else:
+	print("Jsi kluk")
+
+
